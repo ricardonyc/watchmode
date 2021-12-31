ec719e58fd860fe0e9179ed0fb00ae0e9e58d390
ricardonyc <rvardo98@gmail.com>
2021-12-31 17:41:25 -0500

    randomized SVG selection

--- styling.js
+++ styling.js
@@ -1,3 +1,8 @@
+//random SVG 
+const randomNum = Math.floor(Math.random() * 5) 
+document.getElementById("svg").src = `images/movie${randomNum}.svg`
+
+
 window.addEventListener("scroll", function () {
     let navContainer = document.querySelector(".nav-container");
     navContainer.classList.toggle("scrolling-active", window.scrollY > 1);

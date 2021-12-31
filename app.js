feaeb02e8d44fac6da09ba45f37915d800fdca11
ricardonyc <rvardo98@gmail.com>
2021-12-31 16:22:54 -0500

    removed styling js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-window.addEventListener("scroll", function () {
-  let navContainer = document.querySelector(".nav-container");
-  navContainer.classList.toggle("scrolling-active", window.scrollY > 1);
-});
-
 const buttons = document.querySelectorAll(".btn");
 
 // make multiple buttons
@@ -103,10 +98,3 @@ function getRandomElements(array, amount) {
   }
   return newArr;
 }
-
-// menu
-const menuBtn = document.querySelector(".btn");
-
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-});

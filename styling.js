e0cdbfc6bcd1628cbfb47a90c79e59482af4ef3d
ricardonyc <rvardo98@gmail.com>
2021-12-31 22:01:48 -0500

    reorganized order of code

--- styling.js
+++ styling.js
@@ -1,16 +1,17 @@
-//random SVG 
-const randomNum = Math.floor(Math.random() * 5) 
-document.getElementById("svg").src = `images/movie${randomNum}.svg`
-
-
+// navigation bar animation
 window.addEventListener("scroll", function () {
     let navContainer = document.querySelector(".nav-container");
     navContainer.classList.toggle("scrolling-active", window.scrollY > 1);
 });
 
-
+//burger menu
 const menuBtn = document.querySelector(".btn");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("open");
 })
+
+
+//random SVG 
+const randomNum = Math.floor(Math.random() * 5) 
+document.getElementById("svg").src = `images/movie${randomNum}.svg`
